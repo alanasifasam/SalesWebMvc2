a42b59514908128629c35b65a3f50403a5a5b9db
Alana <alanasifasam@gmail.com>
2021-12-13 11:49:49 -0300

    CRUD SCAFFOLDING

---
+++ SalesWebMvc2/Data/SalesWebMvc2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc2.Models;
+
+namespace SalesWebMvc2.Data
+{
+    public class SalesWebMvc2Context : DbContext
+    {
+        public SalesWebMvc2Context (DbContextOptions<SalesWebMvc2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc2.Models.Department> Department { get; set; }
+    }
+}
--- SalesWebMvc2/Startup.cs
+++ SalesWebMvc2/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc2.Data;
 
 namespace SalesWebMvc2
 {
@@ -33,6 +35,9 @@ namespace SalesWebMvc2
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvc2Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvc2Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

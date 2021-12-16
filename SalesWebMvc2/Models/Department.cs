c41577b9dfd8cf46e34ead01b640db16a9291bc2
Alana <alanasifasam@gmail.com>
2021-12-16 11:36:32 -0300

    Salles

--- SalesWebMvc2/Models/Department.cs
+++ SalesWebMvc2/Models/Department.cs
@@ -1,9 +1,21 @@
-﻿
+﻿using System.Collections.Generic;
 namespace SalesWebMvc2.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
---
+++ SalesWebMvc2/Models/Enums/SaleStatus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SalesWebMvc2.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+
+    }
+}

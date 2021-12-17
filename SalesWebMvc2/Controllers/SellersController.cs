842314132707176421f3acb2e11fbef5ecd2b0ac
Alana <alanasifasam@gmail.com>
2021-12-17 13:17:27 -0300

    Controller Sellers

---
+++ SalesWebMvc2/Controllers/SellersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc2.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

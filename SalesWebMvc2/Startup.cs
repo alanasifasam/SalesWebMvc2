30519c0e437a0132461a05e48a3bde6d6868361e
Alana <alanasifasam@gmail.com>
2021-12-17 12:30:39 -0300

    Populando banco de dados

--- SalesWebMvc2/Startup.cs
+++ SalesWebMvc2/Startup.cs
@@ -39,14 +39,17 @@ namespace SalesWebMvc2
             services.AddDbContext<SalesWebMvc2Context>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvc2Context"), builder =>
                       builder.MigrationsAssembly("SalesWebMvc2")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {

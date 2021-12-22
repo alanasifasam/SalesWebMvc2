d2a574577e5f9e266f99f266320f372bfd2b8d05
Alana <alanasifasam@gmail.com>
2021-12-22 13:43:07 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc2/Controllers/SalesRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc2.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}

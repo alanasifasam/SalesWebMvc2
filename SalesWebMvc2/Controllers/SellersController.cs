368c8e9d92c62235695a4ba8c625cc4fb4dfa474
Alana <alanasifasam@gmail.com>
2021-12-22 12:18:09 -0300

    Validation

--- SalesWebMvc2/Controllers/SellersController.cs
+++ SalesWebMvc2/Controllers/SellersController.cs
@@ -38,6 +38,12 @@ namespace SalesWebMvc2.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -97,7 +103,13 @@ namespace SalesWebMvc2.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
